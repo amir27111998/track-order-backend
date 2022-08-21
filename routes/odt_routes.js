1f8ada5bc7268cf157bbd2d6a9989a07b34a5fd6
amir27111998 <alisyedaamir123@hotmail.com>
2022-08-22 03:23:37 +0500

    Adding Get Request

--- routes/odt_routes.js
+++ routes/odt_routes.js
@@ -3,6 +3,7 @@ const { actionsMapping } = require("../controllers/ODTController");
 
 const router = express.Router();
 
+router.get("/", (req, res) => res.send("<h1>ODT Web Hook Is Active</h1>"));
 router.post("/", actionsMapping);
 
 module.exports = {
